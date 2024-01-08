bb819ecb90828d6c187e093ab7f9e8fd0c6d960e
subhakant31 <subhakanta.mishra3105@gmail.com>
2024-01-08 16:06:38 +0530

    left control panel

---
+++ src/components/Organisms/ProductControlPanel/ProductControlPanel.jsx
@@ -0,0 +1,12 @@
+import StyledControlPanel from './ProductControlPanel.styled';
+
+function ProductControlPanel (props) {
+
+    return(
+        <div className='control-panel'>
+            
+        </div>
+    )
+}
+
+export default ProductControlPanel;
