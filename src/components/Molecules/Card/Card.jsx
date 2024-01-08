beaee802e0864f56469c1faac2ea3696c4237206
subhakant31 <subhakanta.mishra3105@gmail.com>
2024-01-08 16:06:25 +0530

    card component UI fixes

--- src/components/Molecules/Card/Card.jsx
+++ src/components/Molecules/Card/Card.jsx
@@ -1,14 +1,26 @@
-import {StyledCard }from "./Card.styled"
-
-export const Card = () => {
-    return (
-      <>
+import { StyledCard } from "./Card.styled";
+import { centsToDollars } from "../../../helperFunctions";
+export const Card = ({
+  image,
+  altText,
+  title,
+  price,
+  productId,
+  brandName,
+}) => {
+  return (
+    <>
       <StyledCard>
-         <img  src='./assests/icons/email.png' alt='dummy image' />
-        <div className=''>
-        </div>
-      </StyledCard>
-      </>
-);
-};
+        <a href='/' className='product' data-id={productId}>
+          <div className='image-container'>
+            <img src={image} alt={altText} />
+          </div>
+          <span className='title'>{title}</span>
+          <span className='brand'> {brandName}</span>
 
+          <span className='price'>${centsToDollars(price)}</span>
+        </a>
+      </StyledCard>
+    </>
+  );
+};
--- src/components/Molecules/Card/Card.styled.js
+++ src/components/Molecules/Card/Card.styled.js
@@ -1,6 +1,45 @@
 import styled from "styled-components";
 
 export const StyledCard = styled.div`
+  .product {
+    text-decoration: none;
+    background-color: #f0f0f0;
 
-
+    .image-container {
+      max-width: 300px;
+      img {
+        width: 100%;
+        object-fit: cover;
+      }
+    }
+    .title {
+      display: block;
+      font-family: Satoshi;
+      font-size: 20px;
+      font-weight: 700;
+      line-height: 27px;
+      letter-spacing: 0em;
+      color: #000;
+    }
+    .brand {
+      display: block;
+      font-family: Satoshi;
+      font-size: 16px;
+      font-weight: 400;
+      line-height: 27px;
+      letter-spacing: 0em;
+      color: #000;
+    }
+    .price {
+      display: block;
+      font-family: Satoshi;
+      font-size: 24px;
+      font-weight: 700;
+      line-height: 32px;
+      letter-spacing: 0em;
+      text-align: left;
+      color: #000;
+      margin-top: 10px;
+    }
+  }
 `;
