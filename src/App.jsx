33f2b9ed590b9303d5e17ff862642a54db1840fa
subhakant31 <subhakanta.mishra3105@gmail.com>
2024-01-09 17:03:07 +0530

    route for products list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./components/RootLayout";
 import HeroBanner from "./components/Organisms/HeroBanner/HeroBanner";
@@ -7,35 +8,33 @@ import Header from "./components/Organisms/Header/Header";
 import { GlobalStyles } from "./styles/globalStyles";
 import Footer from "./components/Organisms/Footer/Footer";
 import "./App.css";
+import HomePage from "./components/Pages/HomePage/HomePage";
+import { ProductContext } from "./contexts/productContext";
 import ProductsList from "./components/Pages/ProductsList/ProductsList";
 import { Card } from "./components/Molecules/Card/Card";
+
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <RootLayout />,
-      children: [
-        {
-          path: "/",
-        },
-        {
-          path: "/",
-        },
-      ],
-    },
-  ]);
+  const [productData, setProductData] = useState([]);
 
   return (
-    <>
-      <GlobalStyles />
-      <Header></Header>
-      {/* <HeroBanner></HeroBanner> */}
-      <ProductsList></ProductsList>
-      <Footer />
-    </>
-    // <div>
-    //   <RouterProvider router={router} />
-    // </div>
+    <Router>
+      <>
+        <GlobalStyles />
+        <Header></Header>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route
+            path='/products'
+            element={
+              <ProductContext.Provider value={[productData, setProductData]}>
+                <ProductsList></ProductsList>
+              </ProductContext.Provider>
+            }
+          />
+        </Routes>
+        <Footer />
+      </>
+    </Router>
   );
 }
 
