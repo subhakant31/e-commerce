eae8eaf7738cac791a4ecd38f47ec4ca537eacef
subhakant31 <subhakanta.mishra3105@gmail.com>
2024-01-09 18:18:50 +0530

    used new param names

--- src/components/Atoms/Heading/Heading.jsx
+++ src/components/Atoms/Heading/Heading.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { StyledHeading } from "./Heading.styled";
-export const Heading = ({ text, primary, centeredText, ...props }) => {
+export const Heading = ({ text, size, centeredText, ...props }) => {
   return (
-    <StyledHeading primary={primary} centeredText={centeredText}>    
-    {(primary === 'large') ? (
+    <StyledHeading size={size} centeredText={centeredText}>    
+    {(size === 'large') ? (
         <h1 className='heading'>{text}</h1>
-      ) : (primary === 'medium') ? (<h2 className='heading'>{text}</h2>
+      ) : (size === 'medium') ? (<h2 className='heading'>{text}</h2>
       ) : (<h3 className='heading'>{text}</h3>)}
     </StyledHeading>
   );
@@ -14,6 +14,6 @@ export const Heading = ({ text, primary, centeredText, ...props }) => {
 
 Heading.propTypes = {
   text: PropTypes.string.isRequired,
-  primary: PropTypes.oneOf(["large", "medium", "small"]),
+  size: PropTypes.oneOf(["large", "medium", "small"]),
   centeredText: PropTypes.bool.isRequired,
 };
--- src/components/Atoms/Heading/Heading.styled.js
+++ src/components/Atoms/Heading/Heading.styled.js
@@ -2,10 +2,18 @@ import styled from "styled-components";
 
 export const StyledHeading = styled.div`
   .heading {
-    font-size: ${(props) => ((props.primary === 'large') ? "64px" : 
-    (props.primary === 'medium') ? "40px" : "20px")};
-    line-height: ${(props) => ((props.primary === 'large') ? "64px" :
-    (props.primary === 'medium') ? "50px" : "30px")};
+    font-size: ${(props) =>
+      props.size === "large"
+        ? "64px"
+        : props.size === "medium"
+        ? "40px"
+        : "20px"};
+    line-height: ${(props) =>
+      props.size === "large"
+        ? "64px"
+        : props.size === "medium"
+        ? "50px"
+        : "30px"};
     font-family: Satoshi;
     font-weight: 700;
     letter-spacing: 0em;
