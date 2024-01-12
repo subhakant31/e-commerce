5ce2a4d8ac5e8903c241ed9709aa9a23520b324f
subhakant31 <subhakanta.mishra3105@gmail.com>
2024-01-12 11:11:45 +0530

    using useMemo

--- src/components/Atoms/OrderSummaryCard/OrderSummaryCard.jsx
+++ src/components/Atoms/OrderSummaryCard/OrderSummaryCard.jsx
@@ -1,47 +1,51 @@
-import { StyledOrderSummary } from './OrderSummaryCard.styled';
+import { StyledOrderSummary } from "./OrderSummaryCard.styled";
 import TextParagraph from "../../Atoms/TextParagraph/TextParagraph";
-import { InputField } from '../InputField/InputField';
-import { Button } from '../Button/Button';
-import { UserCartContext } from '../../../contexts/userCartContext';
-import { useContext } from 'react';
-import { centsToDollars } from '../../../helperFunctions';
+import { InputField } from "../InputField/InputField";
+import { Button } from "../Button/Button";
+import { UserCartContext } from "../../../contexts/userCartContext";
+import { useContext, useMemo } from "react";
+import { centsToDollars } from "../../../helperFunctions";
+
 const OrderSummaryCard = () => {
-        const [userCart, setUserCart] = useContext(UserCartContext);
-        
-        function findTotalAmount(userCart) {
-            let totalSum = 0;
-            let itemSum = 0;
-            for(var i=0;i<userCart.length;i++) {
-                itemSum += +(centsToDollars(userCart[i].item.retail_price_cents)*userCart[i].quantity);
-                totalSum += itemSum;
-                itemSum =0;
-            }
-            return totalSum.toFixed(2);
-        }
-    return (
-        <StyledOrderSummary>
-            <TextParagraph text={'Order Summary'} subText={true} />
-            <div className="order-details">
-                <p>Subtotal: <span>${findTotalAmount(userCart)}</span></p>
-                <p>Discount: <span>0</span></p>
-                <p>Delivery Fee: <span>0</span></p>
-                <p>Total: <span>${findTotalAmount(userCart)}</span></p>
-            </div>
-            <div className='order-button-section'>
-                <div className='order-button'>
-                    <InputField />
-                    <Button 
-                      text='Apply'
-                      className=''
-                    />
-                </div>
-                <Button 
-                 text='Go to Checkout'
-                 className=''
-                />
-            </div>
-        </StyledOrderSummary>
-    );
+  const [userCart] = useContext(UserCartContext);
+
+  const memoizedTotalAmount = useMemo(() => {
+    return userCart
+      .reduce((totalSum, cartItem) => {
+        const itemSum = +(
+          centsToDollars(cartItem.item.retail_price_cents) * cartItem.quantity
+        );
+        return totalSum + itemSum;
+      }, 0)
+      .toFixed(2);
+  }, [userCart]);
+
+  return (
+    <StyledOrderSummary>
+      <TextParagraph text={"Order Summary"} subText={true} />
+      <div className='order-details'>
+        <p>
+          Subtotal: <span>${memoizedTotalAmount}</span>
+        </p>
+        <p>
+          Discount: <span>0</span>
+        </p>
+        <p>
+          Delivery Fee: <span>0</span>
+        </p>
+        <p>
+          Total: <span>${memoizedTotalAmount}</span>
+        </p>
+      </div>
+      <div className='order-button-section'>
+        <div className='order-button'>
+          <InputField />
+          <Button text='Apply' className='' />
+        </div>
+        <Button text='Go to Checkout' className='' />
+      </div>
+    </StyledOrderSummary>
+  );
 };
 
 export default OrderSummaryCard;
